fedaafa7055feae573d4f77739158a767cdf6152
deno.bun <deno.bun@yandex.com>
2025-12-31 16:57:27 +0400

    добавление pkg-config openssl для lettre

--- arch.sh
+++ arch.sh
@@ -1,4 +1,5 @@
-pacman -Syu git openssh docker docker-compose unzip rustup gcc cloc fish fastfetch nano
+# pkg-config openssl - нужен для либы rust (вроде lettre)
+pacman -Syu git openssh docker docker-compose unzip rustup gcc just cloc fish fastfetch nano pkg-config openssl
 
 git config --global user.name "deno.bun"
 git config --global user.email "deno.bun@yandex.com"
@@ -14,10 +15,10 @@ read -r -p "### Нажми Enter, когда закончишь... " < /dev/tty
 systemctl start docker
 systemctl enable docker
 
-go install github.com/air-verse/air@latest
-
 chsh -s /usr/bin/fish
 
 curl -fsSL https://bun.sh/install | bash
 
 rustup default stable
+cargo install cargo-watch
+cargo install sqlx-cli --no-default-features --features postgres
